63636eab1583b51472eed069abc09045f82b5ffd
Ty Benmark <ty.benmark@gmail.com>
2013-03-17 23:19:45 -0300

    Update SumOfDigits.rb
    Given a positive integer, find the sum of its constituent digits.

--- SumOfDigits.rb
+++ SumOfDigits.rb
@@ -1,3 +1,4 @@
+# Given a positive integer, find the sum of its constituent digits.
 File.open(ARGV[0]).each_line do |i|
     puts i.split('').inject(0) { |result,element| result + element.to_i}
 end
