bdf46d3ccb2d12744208bfa850580b86dffe05ac
Ty Benmark <ty.benmark@gmail.com>
2013-03-17 23:29:33 -0300

    Update and rename MultiplesOfANumber to MultiplesOfANumber.rb
    Given numbers x and n, where n is a power of 2, 
    print out the smallest multiple of n which is 
    greater than or equal to x.

--- MultiplesOfANumber.rb
+++ MultiplesOfANumber.rb
@@ -1,4 +1,4 @@
-#Given numbers x and n, where n is a power of 2, 
+#Given numbers x and n, where n is a power of 2,
 #print out the smallest multiple of n which is greater than or equal to x.
 File.open(ARGV[0]).each_line do |i|
     x,n = i.split(',').map(&:to_i)
