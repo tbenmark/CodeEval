34ab4e7964e02e29104f617806b95dd3fb5b3fa8
Ty Benmark <ty.benmark@gmail.com>
2013-03-17 23:22:40 -0300

    Update fib.rb
    The Fibonacci series is defined as: F(0) = 0; F(1) = 1; 
    F(n) = F(n-1) + F(n-2) when n>1;. 
    Given a positive integer 'n', print out the F(n).

--- fib.rb
+++ fib.rb
@@ -1,3 +1,5 @@
+#The Fibonacci series is defined as: F(0) = 0; F(1) = 1; F(n) = F(n-1) + F(n-2) when n>1;. 
+#Given a positive integer 'n', print out the F(n).
 def fib(n)
   return n if n < 2
   fib(n-1) + fib(n-2)
